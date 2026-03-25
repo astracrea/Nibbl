2e843c5282067cd532f9a5ff6368fa45cbc245ed
Arnav Deore <realarnavdeorehere@gmail.com>
2026-03-25 16:49:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Note: If you are deploying to Netlify, you typically do not need the 'base' property 
-  // (it defaults to '/'). This is usually only needed for GitHub Pages. 
-  // If your site loads with broken assets, try removing this line or changing it to base: '/'
-  base: '/your-repo-name/', 
+  // Notice the 'base' property is completely gone now!
   
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
